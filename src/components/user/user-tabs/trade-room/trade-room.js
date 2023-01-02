377ee1674edcd470e2dbf8320326909c02944c44
Rodrigo Dirube <rodrigodirube5@gmail.com>
2023-01-02 12:09:25 -0300

    trade room first part finished

--- src/components/user/user-tabs/trade-room/trade-room.js
+++ src/components/user/user-tabs/trade-room/trade-room.js
@@ -1,7 +1,8 @@
 import React, { useState } from "react"
 import "./trade-room.css"
 import profile from '../../../../assets/images/user1_profile.png'
-
+import door from '../../../../assets/images/door.png'
+import { height } from "@mui/system"
 
 
 function TradeRoom() {
@@ -22,8 +23,8 @@ function TradeRoom() {
                             <div>
                             <span style={{fontWeight:'800', fontSize:'2.2vw'}}>USERNAME</span>
                             </div>
-                            <div style={{transform:'translateY(-1vw)'}}>
-                            <span style={{fontSeize:'1.2vw'}}>0xb42...3b3c</span>
+                            <div>
+                            <span style={{fontSize:'1.2vw'}}>0xb42...3b3c</span>
                             </div>
                         </div>
                     </div>
@@ -37,6 +38,16 @@ function TradeRoom() {
                 </div>
                </div>
                <div className="enter-container">
+                 <div className="enter-inner-container">
+                 <div className="enter-content-container">
+                   <div style={{width:'100%', height:'70%'}}>
+                     <img style={{width:'100%', height:'100%'}} src={door}></img>
+                   </div>
+                   <div style={{display:'flex', justifyContent:'center', transform:'translateY(0.2vw)'}}>
+                     <span style={{fontSize:'1.6vw'}}>ENTER</span>
+                   </div>
+                </div>
+                 </div>
                </div>
             </div>
          </div>
